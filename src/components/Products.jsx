2fcb864805f0d2a06147d7869db7ed6f596750ac
goldemore <goldemorone@gmail.com>
2023-07-31 23:01:10 +0400

    img directoriy change

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,4 @@
-
 import data from "./data";
-
 
 const Products = () => {
  
@@ -12,10 +10,11 @@ const product=()=>{
     <main>
       <h2 className="main_header" id="mehsullarimiz">Məhsullarımız</h2>
       
-        <div class="grid_container">
+        <div className="grid_container">
           {data.map((item, index) => (
             <div className="grid_box" key={index} onClick={product}>
-                <img src={item.img} alt="img" />
+                {/* Обновленный путь к изображению */}
+                <img src={process.env.PUBLIC_URL + item.img} alt="img" />
               <p className="title">{item.title}</p>
               {item.discount ? <del className="price_del"><span>{item.price} AZN</span></del> : <span className="price">{item.price} AZN</span>}
               {item.discount && <span className="price_discount">{item.discount} AZN</span>}
