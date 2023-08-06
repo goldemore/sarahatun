9827e401e0257e59b9bf79f18f80a79e154f4fea
goldemore <goldemorone@gmail.com>
2023-08-07 00:58:37 +0400

    Discount

--- src/components/ProductsGridBox.js
+++ src/components/ProductsGridBox.js
@@ -2,24 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProductsGridBox = ({ dataProducts }) => {
-  
   return (
     <Link to={`/product/${dataProducts.id}`} target="_blank">
       <div className="grid_box">
         <img src={dataProducts.image} alt="img" />
         <div className="grid_box_content">
           <p className="title">{dataProducts.title}</p>
-          {dataProducts.discount ? (
-            <del className="price_del">
-              <span>{dataProducts.price} AZN</span>
-            </del>
+
+          {dataProducts.sale_price ? (
+            <div>
+              <del className="price_del">
+                <span>{dataProducts.price}.00 AZN</span>
+              </del>
+              <span className="price">{dataProducts.sale_price}.00 AZN</span>
+            </div>
           ) : (
-            <span className="price">{dataProducts.price} AZN</span>
-          )}
-          {dataProducts.discount && (
-            <span className="price_discount">
-              {dataProducts.sale_price} AZN
-            </span>
+            <span className="price">{dataProducts.price}.00 AZN</span>
           )}
         </div>
       </div>
